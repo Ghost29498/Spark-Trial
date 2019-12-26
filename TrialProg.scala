fb0075c6ce5020e8a89774f1ed77277e523cddb2
Ghost29498 <shriramsubramanianv@gmail.com>
2019-12-26 11:21:27 +0530

    first
    first

--- TrialProg.scala
+++ TrialProg.scala
@@ -5,9 +5,9 @@ object TrialProg extends App {
     .master("local[*]")
     .appName("Spark Word Count")
     .getOrCreate()
-  val name = "F:\\Crayon\\examples-master\\examples-master\\Search\\ReadTrial\\emp_data.csv"
+  val name = "C:\\Users\\ShriramSubramanian\\Documents\\spark-trial\\ReadTrial\\top50.csv"
   val  df = spark.read.format("csv").option("header", "true").load(name)
   println(df.show(10,false))
-
+  df.write.parquet("C:\\Users\\ShriramSubramanian\\Documents\\spark-trial\\ReadTrial\\top50.parquet")
 
 }
